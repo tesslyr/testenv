8643208e55a2c7b6930f6ba2acd552985e2d74eb
tess <tess>
2020-08-12 10:06:57 +0800

    Initial commit

---
+++ src/main/java/com/example/testenvandroid/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.testenvandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("Team 7 Test Android");
+    }
+}
